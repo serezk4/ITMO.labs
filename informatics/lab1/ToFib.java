519304391f87eed0b122afcc05a240f3b178808e
serezk4 <serezadorohin@gmail.com>
2023-10-25 19:34:40 +0300

    updated lab1 for informatics

--- informatics/lab1/ToFib.java
+++ informatics/lab1/ToFib.java
@@ -1,9 +1,7 @@
 package inf.lab1;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -12,49 +10,51 @@ public class ToFib {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        writer.append("enter value to convert: ").flush();
+        writer.append("Перевод из десятичной системы счисления в фибоначчи").append("\n\n").flush();
+        writer.append("введите число в десятичной системе счисления: ").flush();
 
         String input = reader.readLine();
-        if (!input.matches("\\d+")) return;
+        if (!input.matches("\\d+") || Long.parseLong(input) <= 0) {
+            System.err.println("введите число > 0!");
+            return;
+        }
 
         long number = Long.parseLong(input);
+
         List<Long> fib = getFib(number);
-        List<Long> copy = new ArrayList<>(fib);
         int[] used = new int[fib.size()];
 
-        System.out.println(fib);
+        writer.append("ряд Фибоначчи: ").append(fib.toString()).append("\n").flush();
 
         while (number > 0) {
-            if (fib.contains(number)) {
-                int index = fib.indexOf(number);
-                used[index] = 1;
-                fib.set(index, -1L);
+            if (fib.contains(number) && used[fib.indexOf(number)] != 1) {
+                used[fib.indexOf(number)] = 1;
                 break;
             }
 
-            final long fn = number;
-            long approx = fib.stream().filter(val -> val <= fn).mapToLong(l->l).max().orElse(-1);
-            if (approx == -1) return;
+            final long finalNumber = number;
+            long approx = fib.stream().filter(val -> val <= finalNumber && used[fib.indexOf(val)] != 1)
+                    .mapToLong(l -> l).max()
+                    .orElseThrow(NumberFormatException::new);
 
             number -= approx;
-            int index = fib.indexOf(approx);
-            fib.set(index, -1L);
-            used[index] = 1;
+            used[fib.indexOf(approx)] = 1;
         }
 
-        writer.append(IntStream.range(0, used.length).filter(i -> used[i] == 1).mapToObj(copy::get).mapToLong(q->q).mapToObj(String::valueOf).map(q -> q + " ").collect(Collectors.joining()).stripIndent()).append("\n");
+        writer.append("использованные числа: ").append(IntStream.range(0, used.length)
+                .filter(i -> used[i] == 1)
+                .mapToObj(index -> fib.get(index) + " ")
+                .collect(Collectors.joining()).stripIndent()).append("\n");
 
         String result = Arrays.stream(used).mapToObj(String::valueOf).collect(Collectors.joining()) + " ";
-        writer.append("result: ").append(new StringBuilder(result.substring(1, result.lastIndexOf('1')+2)).reverse().toString()).flush();
-
-        System.gc();
+        writer.append("результат: ").append(new StringBuilder(result.substring(0, result.lastIndexOf('1') + 2)).reverse().toString()).flush();
 
         reader.close();
         writer.close();
     }
 
     public static List<Long> getFib(long to) {
-        List<Long> list = new ArrayList<>(List.of(1L,1L));
+        List<Long> list = new ArrayList<>(List.of(1L, 1L));
 
         for (int i = 2; list.get(list.size() - 1) <= to; i++)
             list.add(list.get(i - 2) + list.get(i - 1));
@@ -62,3 +62,4 @@ public class ToFib {
         return list;
     }
 }
+
