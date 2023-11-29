1e47d9b9fc65aae96085c3580c2123c124430d93
serezk4 <serezadorohin@gmail.com>
2023-11-29 18:03:37 +0300

    added readme & updated Main.java

--- informatics/lab4/src/main/java/org/serezka/Main.java
+++ informatics/lab4/src/main/java/org/serezka/Main.java
@@ -1,7 +1,22 @@
 package org.serezka;
 
+
+/**
+ * @variant: 34
+ * @task YAML -> XML, Суббота
+ *
+ * @author serezk4
+ * @subject informatics
+ * @lab 4
+ * @date 29.11.2023
+ */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Main().run();
+    }
+
+    private void run() {
+
     }
 }
