4a1183aebcd33ce89f9a710702a784408db032f8
serezk4 <serezadorohin@gmail.com>
2024-01-25 03:33:09 +0300

    init lab6

---
+++ programming/lab6/client/src/test/java/com/serezka/lab6client/Lab6clientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serezka.lab6client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab6clientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ programming/lab6/server/src/main/java/com/serezka/lab6server/Lab6serverApplication.java
@@ -0,0 +1,13 @@
+package com.serezka.lab6server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab6serverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab6serverApplication.class, args);
+	}
+
+}
