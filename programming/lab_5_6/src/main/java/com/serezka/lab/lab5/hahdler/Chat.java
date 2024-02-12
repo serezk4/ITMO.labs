e142060d5a9492a8b6c00d07f6bed525b2c0bdd0
serezk4 <serezadorohin@gmail.com>
2024-02-12 17:18:14 +0300

    added variant for lab7

--- programming/lab_5_6/src/main/java/com/serezka/lab/lab5/hahdler/Chat.java
+++ programming/lab_5_6/src/main/java/com/serezka/lab/lab5/hahdler/Chat.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+import java.io.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
